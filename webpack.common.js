1f3cd94b251b7b2769175831ffb0e112858a98ef
cakrawalabahasa <officialcakrawalabahasa@gmail.com>
2021-06-26 21:59:15 +0700

    Cache API

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,8 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
- 
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin')
+const path = require('path')
+
 module.exports = {
   entry: path.resolve(__dirname, 'src/scripts/index.js'),
   output: {
@@ -36,5 +37,8 @@ module.exports = {
         },
       ],
     }),
+    new ServiceWorkerWebpackPlugin({
+      entry: path.resolve(__dirname, 'src/scripts/sw.js'),
+    }),
   ],
-};
+}
