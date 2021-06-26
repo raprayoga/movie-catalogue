2ba2fb38eeb5cd47c8d930c08d3827b30c8883b3
cakrawalabahasa <officialcakrawalabahasa@gmail.com>
2021-06-26 13:30:40 +0700

    app shell

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '3d9f9ef2c48e32abf7b8bc701d8c8143',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+  
+export default CONFIG;
